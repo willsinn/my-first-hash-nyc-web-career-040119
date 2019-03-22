f8a2266b19a39fcba018c27cba3d86b78c1e9d75
w.h.sinn <w.h.sinn@gmail.com>
2019-03-22 09:45:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,13 +32,13 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+end
   shipping_manifest["muskets"=> 2,]
   # add 4 gun powder to the shipping_manifest hash below
 
   shipping_manifest["gun powder"=> 4]
 
-
+end
   # return the shipping_manifest hash below
   shipping_manifest
 end
